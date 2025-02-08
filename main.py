a143daf304dfc36db68bb2598c038a9cd85b7939
Andrew <ap762@duke.edu>
2025-02-08 17:31:17 -0500

    changed port

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def search():
                          sharpe_ratio=sharpe_ratio,
                          annual_returns=annual_returns)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
